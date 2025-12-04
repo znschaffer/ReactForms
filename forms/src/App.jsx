c05dfedb150ea91c147cb547e563184bf42c8fff
Zane Schaffer <zschaffer@eventide.com>
2025-12-03 18:23:32 -0800

    finish project

--- forms/src/App.jsx
+++ forms/src/App.jsx
@@ -1,12 +1,11 @@
 import RestaurantsContainer from "./components/RestaurantsContainer";
 import AddRestaurant from "./components/addRestaurant";
-import { data } from "./data/data.js";
 import { useState } from "react";
 import "./App.css";
 
 //App is the root of our application and where we load in our components.
 function App() {
-  const [restaurantState, setRestaurants] = useState([...data]);
+  const [restaurantState, setRestaurants] = useState([]);
 
   const updateRestaurants = (restaurant) => {
     console.log(restaurant);
--- forms/src/components/AddRestaurant.jsx
+++ forms/src/components/AddRestaurant.jsx
@@ -1,35 +1,59 @@
-import { useState } from "react";
-function AddRestaurant({ updateRestaurants }) {
+import {useState} from "react";
+
+function AddRestaurant({updateRestaurants}) {
+  const [formData, setFormData] = useState({
+    name: "",
+    image: "",
+    address: "",
+    phone: "",
+    cuisine: "",
+    rating: "",
+  })
+
+  const handleChange = (e) => {
+    const name = e.target.name
+    const value = e.target.value
+    setFormData(prev => {
+      return {...prev, [name]: value}
+    })
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    updateRestaurants(formData)
+  }
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <div>
         <label>Name:</label>
-        <input type="text" name="name" required />
+        <input onChange={handleChange} value={formData.name} type="text" name="name" required/>
       </div>
 
       <div>
         <label>image:</label>
-        <input type="text" name="image" required />
+        <input onChange={handleChange} value={formData.image} type="text" name="image" required/>
       </div>
 
       <div>
         <label>Address:</label>
-        <input type="text" name="address" required />
+        <input onChange={handleChange} value={formData.address} type="text" name="address" required/>
       </div>
 
       <div>
         <label>Phone:</label>
-        <input type="tel" name="phone" required />
+        <input onChange={handleChange} value={formData.phone} type="tel" name="phone" required/>
       </div>
 
       <div>
         <label>Cuisine:</label>
-        <input type="text" name="cuisine" required />
+        <input onChange={handleChange} value={formData.cuisine} type="text" name="cuisine" required/>
       </div>
 
       <div>
         <label>Rating:</label>
-        <input type="number" name="rating" min="1" max="5" required />
+        <input onChange={handleChange} value={formData.rating} type="number" name="rating" min="1" max="5" required/>
       </div>
 
       <button type="submit">Submit</button>
